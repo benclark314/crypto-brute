4a2b2fe2b76d4524ccaaccd7c1fbe2f720bd4e8d
Ben Clark <benclark314@gmail.com>
2022-12-07 01:08:36 -0600

    Attempt with permutations of different moduli

--- crypto-brute.py
+++ crypto-brute.py
@@ -1,20 +1,71 @@
 #import math
 import time
+import math
+import itertools
+#from itertools import permutations
+#from unittest import result
 ##########################################
 #Function Definitions:
 ##########################################
-from unittest import result
 
-def get_res(ciphertext, indexArray):
-    result = ""
-    for x in range(0, len(ciphertext)):
-        index = indexArray.index(x)
-        result = result + ciphertext[index]
-    return result    
+def get_all_strings_by_regular_moduli_in_range_inclusive(ciphertext, lower, upper):
+    allresults = []
+    for x in range(lower, upper + 1):
+        array = compute_array(ciphertext, x)
+        stringsarray = compute_strings_array(array)
+        allresults.extend(stringsarray)
+    return allresults
 
-def get_skip_indices(inputstring, skipNumber):
+def compute_strings_array(array):
+    permsarrayindices = []
+    permsarray = []
+    results = []
+
+    for x in range(0, len(array)):
+        permsarrayindices.append(x)
+        
+    permsarray = list(itertools.permutations(permsarrayindices))
+    #print(permsarray)
+    for y in range(0, len(permsarray)):
+        count = 0
+        flag = True
+        result_string = ""
+        string_bound_exceeded = 0
+        while(flag):
+            perms_index = count % len(permsarray[y])
+            string_index = math.ceil((count + 1) / (len(permsarray[y])))
+            main_index = permsarray[y][perms_index]
+            # print("array: ")
+            # print(array)
+            # print("\n main_index: ")
+            # print(str(main_index))
+            # print("\n string_index: ")
+            # print(str(string_index))
+            if(len(array[main_index]) > string_index - 1):
+                result_string = result_string + array[main_index][string_index - 1]
+            else:
+                string_bound_exceeded = string_bound_exceeded + 1
+            count = count + 1
+            if(string_bound_exceeded == len(array)):
+                results.append(result_string)
+                flag = False
+    return results
+    #print(results)
+
+#prints several arrays, each one index further up the input string. EG: abcdefghi -> [a, d, g], [b, e, h], [c, f, i]
+def compute_array(inputstring, numberOfArrays):
+    completedSubstrings = []
+    for w in range(0, numberOfArrays + 1):
+        completedSubstrings.append("")
+    for x in range(0, len(inputstring)):
+        index = x % (numberOfArrays + 1)
+        completedSubstrings[index] = completedSubstrings[index] + ciphertext[x]
+    #print(completedSubstrings)
+    return completedSubstrings
+
+def compute_skip(inputstring, skipNumber):
     completedNumbers = []
-    resultIndices = []
+    resultstring = ""
     for x in range(0, len(inputstring)):
         index = skipNumber*x % len(inputstring)
 
@@ -26,8 +77,8 @@ def get_skip_indices(inputstring, skipNumber):
 
             completedNumbers.append(index)
         if(index < len(inputstring)):
-            resultIndices.append(index)
-    return resultIndices
+            resultstring = resultstring + inputstring[index]
+    return resultstring
 
 def find_sub_words(english_words, ciphertext):
     containedWords = []
@@ -49,45 +100,27 @@ def load_words():
 #if __name__ == '__main__':
 starttime = time.perf_counter()
 
-
-
-
-#Attempt to reverse, essentially rotate the index mapping by 90 degrees clockwise.
-#Attempted to accomplish this by using arrays of indices, and then mapping ciphertext to index of index in that array.
-#.......didn't work!
-
-
+#SKIP BY REGULAR MODULI:
 english_words = load_words()
 f = open("ciphertext.txt", "r")
 ciphertext = f.read()
 f = open("results.txt", "a")
 summary = []
 f.write("Original String: " + ciphertext + "\n")
-for w in range(0, len(ciphertext)):
-    if (w != 0):
-        #Shift the ciphertext over one to the right, bringing rightmost element to front:
-        ciphertext = ciphertext[-1] + ciphertext[0: len(ciphertext) - 1]
-    for x in range(1, len(ciphertext)):
-        reso = get_skip_indices(ciphertext, x)
-        res = get_res(ciphertext, reso)
-
-
-        # ssef = ""
-        # for ind in range(0, len(reso)):
-        #     ssef = ssef + ", " + str(reso[ind])
-        # f.write("Index array " + ssef + "\n")
-
-
-        f.write("Inverse shift by " + str(w) + " and skip by " + str(x) + " results in: " + res + "\n")
-        words = find_sub_words(english_words, res)
-        wordcount = len(words)
-        f.write("\tTransform resulted in " + str(wordcount) + " words.\n")
-        for y in range(0, wordcount):
-            f.write("\t" + words[y] + "\n")
-        summary.append(str(wordcount) + " English words " + "are found with Shift by " + str(w) + " and skip by " + str(x) + "\n")
+stngs = get_all_strings_by_regular_moduli_in_range_inclusive(ciphertext, 6, 8)
+for stng in stngs:
+    f.write("String: " + stng + "\n")
+    words = find_sub_words(english_words, stng)
+    wordcount = len(words)
+    f.write("\t Had " + str(wordcount) + " words.\n")
+    for y in range(0, wordcount):
+        f.write("\t" + words[y] + "\n")
+    summary.append(str(wordcount) + " English words " + "are found in string " + stng + "\n")
 summary.sort()
 for z in summary:
     f.write(z)
 endtime = time.perf_counter()
 f.write("runtime was " + str(endtime - starttime) + " seconds.")
 f.close()
+
+
